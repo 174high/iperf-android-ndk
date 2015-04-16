89e54732f78c9a79c1a3a0ce841e574c69f064ae
174high <466243346@qq.com>
2015-04-16 18:27:44 +0800

    iperf android

---
+++ iperf-2.0.5/build.sh
@@ -0,0 +1,3 @@
+./configure --host=arm-linux-gnueabihf CXX=arm-linux-androideabi-g++ CXXFLAGS=-static CC=arm-linux-androideabi-gcc CFLAGS=--sysroot=/home/snq/rowboat-android/wl18xx-snq/android-ndk-r9d/platforms/android-17/arch-arm/
+
+# ./configure   CC=arm-linux-androideabi-gcc  CFLAGS=--sysroot=/home/snq/rowboat-android/wl18xx-snq/android-ndk-r9d/platforms/android-17/arch-arm/
